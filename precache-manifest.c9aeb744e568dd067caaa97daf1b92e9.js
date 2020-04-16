64756c116dfaa5c472d5c469da4599f467d44958
tpsteiner <thomaspsteiner1@gmail.com>
2020-04-15 23:50:30 -0400

    Updates

---
+++ precache-manifest.c9aeb744e568dd067caaa97daf1b92e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3467ae16035616e2077ebdc5027cde",
+    "url": "/animate-svg/index.html"
+  },
+  {
+    "revision": "b0c1ec846f84acc06e23",
+    "url": "/animate-svg/static/css/main.3513a344.chunk.css"
+  },
+  {
+    "revision": "343c4ee77c0be8bcf988",
+    "url": "/animate-svg/static/js/2.22b8f92d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animate-svg/static/js/2.22b8f92d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0c1ec846f84acc06e23",
+    "url": "/animate-svg/static/js/main.ab03f45a.chunk.js"
+  },
+  {
+    "revision": "bb08ad7f98429078f08d",
+    "url": "/animate-svg/static/js/runtime-main.ba8718b5.js"
+  }
+]);
